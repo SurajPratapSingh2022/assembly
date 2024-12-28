121014520201d413239a462f7d05268b76259ace
surajpratapsingh2022 <surajpratapsingh2022@gmail.com>
2024-12-28 21:15:12 +0530

    first asm code

---
+++ msgPrint.asm
@@ -0,0 +1,26 @@
+;write a program that  display a message on a screen     
+
+.model small    ;model directive
+.stack 100h     ;stack segment 100h
+.data
+    ;variables can be declared
+    msg db 'Welcome to Assembly Language Program$'
+    msg1 db 0dh,0ah,'Hello SPS$'
+.code 
+    main proc
+        mov ax,@data
+        mov ds,ax
+        ;display a string
+        mov ah,09h
+        ;mov dx,offset msg
+        lea dx,msg  ;lea load effective address(loads the destination with address of source)
+        int 21h
+        mov ah,09h
+        ;mov dx,offset msg1
+        lea dx,msg1
+        int 21h
+        ;terminate a program
+        mov ah,4ch
+        int 21h
+    main endp
+    end main
